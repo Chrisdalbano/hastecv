41c3ddc690876d72fc158205ea3d6dd4ee697fcb
David Jesús Figuera Marquez <daavidjesuus2019@gmail.com>
2024-08-10 22:40:18 +0200

    COMPLETE REFACTOR

--- pdfgen-front/src/views/HomeView.vue
+++ pdfgen-front/src/views/HomeView.vue
@@ -1,46 +1,41 @@
 <template>
-  <div class="center relative z-10 grid grid-cols-2 gap-4 overflow-hidden py-4">
+  <div
+    style="--min-width: 300"
+    class="center relative z-10 grid grid-cols-2 gap-4 overflow-hidden py-4 max-md:grid-cols-1"
+  >
     <div>
-      <!-- refactor -->
       <TemplateSelector @select="updateTemplate" />
-
-      <ResumeForm @submit="generatePdf" />
+      <div class="switch-toggle">
+        <AppLink to="/" class="flex-1">Manual</AppLink>
+        <AppLink to="/json" class="flex-1">JSON</AppLink>
+      </div>
+      <RouterView></RouterView>
     </div>
-    <PreviewCV />
+    <PreviewCV class="min-w-[--min-width]" :downloadLink="store.downloadLink" />
   </div>
-  <DownloadLink v-if="downloadLink" :downloadLink="downloadLink" />
+  <DownloadLink v-if="store.downloadLink" :downloadLink="store.downloadLink" />
 </template>
 <script setup>
-import ResumeForm from "@/components/home/ResumeForm.vue";
 import DownloadLink from "@/components/DownloadLink.vue";
 import TemplateSelector from "@/components/home/TemplateSelector.vue";
 import PreviewCV from "@/components/home/PreviewCV.vue";
+import AppLink from "@/components/AppLink.vue";
 
-import { ref } from "vue";
+import { useResumeDataStore } from "../stores/resumeData";
 
-const downloadLink = ref(null);
-const template = ref("default");
-
-async function generatePdf(jsonData) {
-  try {
-    const response = await fetch("http://127.0.0.1:5000/generate", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ data: jsonData, template: template.value })
-    });
-
-    if (!response.ok) throw new Error("Failed to generate PDF");
-
-    const blob = await response.blob();
-    downloadLink.value = URL.createObjectURL(blob);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-}
+const store = useResumeDataStore();
 
 function updateTemplate(newTemplate) {
-  template.value = newTemplate;
+  store.template = newTemplate;
 }
 </script>
+
+<style scoped>
+.switch-toggle {
+  --max-width: 260px;
+  background: transparent;
+  display: flex;
+  max-width: var(--max-width);
+  margin-top: 1em;
+}
+</style>
