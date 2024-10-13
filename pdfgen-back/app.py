d992e9434acf4798fa2080e4f75b434daddcbd93
Chrisdalb <chrisdalbano12@gmail.com>
2024-10-13 18:47:08 -0400

    Updated for vercel and heroku

--- pdfgen-back/app.py
+++ pdfgen-back/app.py
@@ -5,8 +5,9 @@ from flask_talisman import Talisman
 import json
 import logging
 
-
-app = Flask(__name__, static_folder='dist', template_folder='dist')
+app = Flask(
+    __name__, static_folder="dist", template_folder="dist"
+)  # Serving frontend from 'dist'
 CORS(app, resources={r"/generate": {"origins": "https://hastecv.com"}})
 Talisman(app)
 
@@ -16,17 +17,18 @@ handler.setLevel(logging.ERROR)
 app.logger.addHandler(handler)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")  # Serve the Vue.js app
 
 
-@app.route('/generate', methods=['POST'])
+# Other routes for API functionality
+@app.route("/generate", methods=["POST"])
 def generate():
     try:
         req_data = request.get_json()
-        data = req_data.get('data')
-        template = req_data.get('template', 'default')
+        data = req_data.get("data")
+        template = req_data.get("template", "default")
 
         if isinstance(data, str):
             data = json.loads(data)
@@ -35,7 +37,7 @@ def generate():
             raise ValueError("Invalid data format")
 
         generate_resume(data, template=template)
-        return send_file('resume.pdf', as_attachment=True)
+        return send_file("resume.pdf", as_attachment=True)
 
     except ValueError as ve:
         app.logger.error(f"ValueError: {ve}")
@@ -45,22 +47,22 @@ def generate():
         return jsonify({"error": "An unexpected error occurred"}), 500
 
 
-@app.route('/templates')
+@app.route("/templates")
 def get_templates():
     templates = ["default", "modern", "minimal"]
     return jsonify(templates)
 
 
-@app.route('/<path:path>')
+@app.route("/<path:path>")
 def static_proxy(path):
     return app.send_static_file(path)
 
 
 @app.after_request
 def set_secure_headers(response):
-    response.headers['Content-Security-Policy'] = "default-src 'self';"
-    response.headers['X-Content-Type-Options'] = 'nosniff'
-    response.headers['X-Frame-Options'] = 'DENY'
+    response.headers["Content-Security-Policy"] = "default-src 'self';"
+    response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["X-Frame-Options"] = "DENY"
     return response
 
 
