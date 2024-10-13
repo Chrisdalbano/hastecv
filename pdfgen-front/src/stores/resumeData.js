162268ab78eb0954921dd1aff7b2ceea784653b3
Chrisdalb <chrisdalbano12@gmail.com>
2024-10-13 19:08:59 -0400

    Updated endpoint for resumeData.js

--- pdfgen-front/src/stores/resumeData.js
+++ pdfgen-front/src/stores/resumeData.js
@@ -66,13 +66,16 @@ export const useResumeDataStore = defineStore("resume", () => {
     }
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/generate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
-        },
-        body: JSON.stringify({ data: jsonData, template: template.value })
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_BASE_URL}/generate`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({ data: jsonData, template: template.value })
+        }
+      );
 
       if (!response.ok) throw new Error("Failed to generate PDF");
 
